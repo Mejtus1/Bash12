fb40ead604efa0472d35f0c189f9ad4be6465faf
Mejtus1 <GitMaster1769@proton.me>
2023-08-01 09:52:32 +0200

    networking commands

--- IBM-bash2.js
+++ IBM-bash2.js
@@ -19,3 +19,24 @@ outputs: Alan Touring 1912
 paste -d "," first.txt last.txt yob.txt
 outputs: Alan,Touring,1912
          Bjarne,Stratoustup,1950
+
+Networking commands 
+hostname - print host name 
+hostname -i = provides IP address of host device
+
+ifconfig (interface configuration) - display or configure the system network interfaces
+ifconfig eth0 (specified ethernet adapter information)
+
+ping - send ICMP packets to URL and print response 
+ping google.com (ping returs a line of info for each succesfull response to an echo request)
+= after that is responds with statistics of a given command
+
+curl (client URL) transfer data to and from URLs
+curl www.google.com = returns the entire landing page contenct of www.google.com using HTTP protocol
+curl www.google.com -o google.txt (we can store contents of a URL to a local file (google.txt))
+
+wget (web get) - download files from a URL 
+wget https://www.google.com/TR/PNG/iso_8859/
+= while downloading it shows us the IP address of server from which we are downloading, 
+HTTP request status, packet length and where is the downloading file saved
+
