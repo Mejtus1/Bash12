ddd1b216b1114ea730373a228d34e3219b52dafb
Mejtus1 <GitMaster1769@proton.me>
2023-08-08 21:25:10 +0200

    file archiving and compressions

--- IBM-bash2.js
+++ IBM-bash2.js
@@ -40,3 +40,28 @@ wget https://www.google.com/TR/PNG/iso_8859/
 = while downloading it shows us the IP address of server from which we are downloading, 
 HTTP request status, packet length and where is the downloading file saved
 
+
+
+File archiving and Compression Commands 
+Archiving is a process of storing information and preserving it 
+it is a collection of data files and directories stored as a single file 
+file compression 
+reduces file size by reducing information redundancy
+preserves storage space, speeds up data transfer and reduces bandwidth load
+
+ls -r = directory tree (shows directories with all its subdirectories)
+Archive
+tar (tape archiver) - archive and exract files
+tar -cf notes.tar notes (we have notes file with its subdirectories math and phisics)
+tar -czf notes.tar.gz notes = ensures that windows based system will recognize the fille tpye 
+tar -tf notes.tar = lists all archived folder with subdirectories
+tar -xf notes.tar notes = -x option tells tar to extract files and folders 
+tar -xzf notes.tar.gz notes = decompress the gz file 
+File compression and archiving 
+zip = Compress files and directories to an archive 
+zip compress than bundle 
+tar bundle than compress
+zip -r notes,zip notes = zip notes 
+unzip notes.zip 
+
+
