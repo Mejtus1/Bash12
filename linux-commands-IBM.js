56bc10d9e7863aec76e3e92da944645f525615be
Mejtus1 <GitMaster1769@proton.me>
2023-08-16 19:58:49 +0200

    basic linux commands 2

--- linux-commands-IBM.js
+++ linux-commands-IBM.js
@@ -18,3 +18,33 @@ history - shows history of your commands, might be used to show which commands w
 during operation system uptime 
 history -c = clears history 
 
+-----------------------------------------------------------------------------------------
+Basic Linux commands 
+uname = shows operating system
+uname -a = shows additional information like current Linux kernal used, hostname
+cd = change directory 
+pwd - print working directory = shows information about the path we are currently in 
+
+tar: create a new tar archive
+tar cvf archive_name.tar dirname/
+tar xvf archive_name.tar = exract from an existing tar archive
+tar tvf archive_name.tar = view an existing tar archive 
+
+grep: Search for a given string in a file(case in-sensitive search)
+grep -i "the" demo_file
+grep -A 3 -i "example" demo_file = print the matched line, along with the 3 lines after it 
+grep -r "ramesh" * = search for a given string in all files recursively
+
+find: Find files using file-name ( case in-sensitive find )
+find -iname "MyCProgram.c"
+find -iname "MyCProgram.c" -exec md5sum = execute commands on files found by the find command
+find -empty = find all emptry files in home directory
+
+ls: lists directory contents of files and directories
+ls -ltr = order files based on last modified time 
+
+shutdown: Shutdown the system and turn the power off immediately
+shutdown -h now 
+shutdown -h +10 = shutdown system after 10 minutes 
+shutdown -r now = reboot the system using shutdown command
+shutdown -fr now = force the filesystem check during reboot 
