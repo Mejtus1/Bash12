cc4dbb3d1e1ff1f3ce66be13d007fe414d1b2272
Mejtus1 <GitMaster1769@proton.me>
2023-08-16 19:58:10 +0200

    Basic linux commands

--- linux-commands-IBM.js
+++ linux-commands-IBM.js
@@ -1,1 +1,20 @@
 /*
+Basic linux commands 
+
+Internal commands:
+- built into the shell program and are shell dependent. Also called build-in commands
+- they are already in the system
+
+External commands:
+- Commands that the system offers, are totally shell-independent and usually can be found in any Linux distribution
+- they mostly reside in /bin and /usr/bin
+
+command tricks 
+- command completion = tab key
+- CTRL A = move cursor to the beginning of the file 
+- CTRL E = move your cursor to the end of the line 
+
+history - shows history of your commands, might be used to show which commands were runned 
+during operation system uptime 
+history -c = clears history 
+
