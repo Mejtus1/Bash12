86da0a468408c45a193314179ec938b9f1a6b81d
Mejtus1 <GitMaster1769@proton.me>
2023-08-21 20:57:24 +0200

    bash google tutorial commands

---
+++ bashGoogleCommands.js
@@ -0,0 +1,13 @@
+/*
+Bash = (cmd/powershell in Linux) 
+(you need to write it properly LS / will not work only with ls /)
+
+----------------------------------------------------------------------------------------
+Text editors in Linux 
+1. GNU Emacs text editor (can do almost everything) 
+https://www.gnu.org/software/emacs/tour/
+2. Vim text editor (mech didnt like this one that much) 
+https://vim8.org
+3. NANO text editor (basic text editor for normal functioning, looks very good for first timers, overall really like it) 
+https://www.nano-editor.org
+----------------------------------------------------------------------------------------
