efcc8c77af9afc2e9ebc06fc02bb283a63ad45cc
Mejtus1 <GitMaster1769@proton.me>
2023-08-23 13:23:15 +0200

    adding commands

--- windowsPowershellandCmdGoogleCommands.js
+++ windowsPowershellandCmdGoogleCommands.js
@@ -43,3 +43,36 @@ cp *.jpgC:\Users\Matus\Documents\jpg.files
 example: cp Bird Pictures C:\Users\Matus\Desktop -Recurse -Verbose (directory and all contetnts are copied to the desktop) 
 14.mv (renames document) - mv\Document.txt \Thisisnotdocument.txt
 15. rm (remove files or directories !ONCE the files are removed they cant be brought back)
+
+File and text Manipulation 
+16. cat (concatenate), example: cat .\linux.txt (.\foldername.txt) opens up contents of our shell 
+17. more (is used after cat when shell is too big, you can navigate than throught file with enter - one line and space advances the file 
+by one page, page is structured from size of your terminal window. When you want to leave the file hit q key. 
+18. cat + -hat10 (-hat10 is used at the end of command) to view only header of the file (it shows first 10 lines of the line)
+18. cat + -tail10 (-tail10 shows us last 10 lines of text in shell) 
+examples: cat .\linux.txt -hat10 ......................... cat .\linux.txt -tail10
+19. start + program name we want to open the file with + file name (starts program and opens folder we choose) 
+example: start notepad linux.txt
+
+Difference betweeen cmd commands and powershell commands 
+Get-Help (powershell command) 
+/? (cmd command) 
+both mean the same thing 
+
+Search files, documents and programs in your computer 
+Ctrl f - searches words in document 
+You'll see the Indexing Options in your results of the search, click on that. 
+Now you want to change the settings for the user folder which is where all the home directories are stored. 
+Select Users and then click Advanced.
+Now select the File Types tab, and select Index Properties and File Contents.
+1. Notepad++ search ( If you don't want to use the Windows Search Service, we can also use Notepad++, the editor that we 
+installed in an earlier lesson. From Notepad++, press Ctrl+Shift+F to open the Find in Files dialog.
+20.POWERSHELL searching sls (searches for strings (strings = text)) 
+we write it like this: Select-String + word we are looking for + place where we want to look it up 
+Select-String linux Linux.txt 
+We can use * to search in all documents (for example .txt or ........) 
+Select-String linux *
+
+21. -FIlter (filter parameter when we want to look for example for specific file, text or 
+ls C:\Users\matus\Desktop -Recurse -Filter *.exe
+ls opens our users-matus-desktop, recurse looks inside of everything, filter filters our next command which is *.exe
