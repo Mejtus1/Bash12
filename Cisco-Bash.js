4d21ae871fddee7971e54caeaad5714e0d1f0b55
Mejtus1 <GitMaster1769@proton.me>
2023-09-11 21:44:38 +0200

    full path / relative path

---
+++ Cisco-Bash.js
@@ -0,0 +1,10 @@
+/*
+Copying files
+
+Full path: 
+cp /home/user/Documents/http.pcapng /home/user/
+pcapng has been copied from Documents folder to user directory
+
+Relative path: 
+(Copy to directory where we currently are)
+cp http.pcapng http2.pcapng 
