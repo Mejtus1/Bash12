1ac5da8ff36a1a46554fe5956fd29494e02bfdab
Mejtus1 <GitMaster1769@proton.me>
2023-09-11 21:59:03 +0200

    changing directories

--- Cisco-Bash.js
+++ Cisco-Bash.js
@@ -8,3 +8,23 @@ pcapng has been copied from Documents folder to user directory
 Relative path: 
 (Copy to directory where we currently are)
 cp http.pcapng http2.pcapng 
+
+------------------------------------------------------------------------------
+Changing Directories
+
+cd <destination directory> = change directories
+
+cd /home/<your username>/Downloads = navigate to your Downloads directory
+the benefit of using a fully qualified path
+- execute the command from any location in the file system 
+
+tilde (~)
+cd ~/Downloads = execute the command from any location in the file system
+
+Copying and Moving Files
+cp: Copy files
+mv: Move files
+
+cp /<source path/file name>/<destination path/file name> 
+
+cp Downloads/test.pcap PCAPS 
